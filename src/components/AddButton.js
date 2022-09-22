e25ccbe0863a2d105fe1ee56b29e9cee93f14921
rmuho16 <48476531+rmuho16@users.noreply.github.com>
2022-09-22 18:32:08 +0200

    updated add job ui

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,14 +1,16 @@
-import {useState} from "react"
+import React, {useState} from "react"
 import {useDispatch} from "react-redux"
 import {addJob} from "../redux/jobsSlice"
 
 const AddButton = () => {
     const dispatch = useDispatch()
-    const [company, setCompany] = useState('')
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [label, setLabel] = useState('')
-    const [location, setLocation] = useState('')
+    const [state, setState] = React.useState({
+        company: "",
+        title: "",
+        description: "",
+        label: "",
+        location: "",
+    })
     const [showInputs, setShowInputs] = useState(false)
     const [showInputError, setShowInputError] = useState(false)
     const bookmarked = false
@@ -20,25 +22,14 @@ const AddButton = () => {
         setShowInputs(currInputs => !currInputs)
     }
 
-    const handleCompany = (e) => {
-        setCompany(e.target.value)
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name] : e.target.value
+        })
     }
 
-    const handleTitle = (e) => {
-        setTitle(e.target.value)
-    }
-
-    const handleDescription = (e) => {
-        setDescription(e.target.value)
-    }
-
-    const handleLabel = (e) => {
-        setLabel(e.target.value)
-    }
-
-    const handleLocation = e => {
-        setLocation(e.target.value)
-    }
+    const {company, title, description, label, location} = state
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -57,11 +48,15 @@ const AddButton = () => {
             && label
             && location) {
             dispatch(addJob(newJob))
-            setCompany('')
-            setDescription('')
-            setTitle('')
-            setLabel('')
-            setLocation('')
+
+            setState({
+                company: "",
+                title: "",
+                description: "",
+                label: "",
+                location: "",
+            })
+
             setShowInputs(false)
         } else {
             setShowInputError(true)
@@ -78,64 +73,53 @@ const AddButton = () => {
 
             {/*<FaPlus className='faIcon'/>*/}
             {showInputs &&
-                <form className="needs-validation mt-2" onSubmit={handleSubmit}>
-                    <div className="row g-3 d-flex justify-content-center">
-                        <div className="col-sm-6 col-lg-5">
-                            <label htmlFor="firstName" className="form-label fw-semibold">Company</label>
-                            <input type="text" value={company} onChange={handleCompany} className="form-control rounded-0"
-                                   placeholder="Enter company name..."/>
-                            <div className="invalid-feedback">
-                                Company name is required.
-                            </div>
-                        </div>
-
-                        <div className="col-sm-6 col-lg-5">
-                            <label className="form-label fw-semibold">Job title</label>
-                            <input type="text" className="form-control rounded-0" id="jobTitle"
-                                   placeholder="Enter job title..." value={title} onChange={handleTitle}
-                                   required/>
-                            <div className="invalid-feedback">
-                                Job title is required.
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-lg-5">
-                            <label htmlFor="location" className="form-label fw-semibold">Location</label>
-                            <input className="form-control rounded-0 " id="exampleFormControlTextarea1"
-                                   placeholder="Enter job location..." value={location}
-                                   onChange={handleLocation}/>
-                            <div className="invalid-feedback">
-                                Job location is required.
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-lg-5">
-                            <label htmlFor="label" className="form-label fw-semibold">Label</label>
-                            <input className="form-control rounded-0 " id="exampleFormControlTextarea2"
-                                   placeholder="Enter job label: e.g.(Engineering Jobs)..." value={label}
-                                   onChange={handleLabel}/>
-                            <div className="invalid-feedback">
-                                Job label is required.
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-lg-5">
-                            <label htmlFor="description" className="form-label fw-semibold">Description</label>
-                            <input className="form-control rounded-0 " id="exampleFormControlTextarea3"
-                                   placeholder="Enter job description..." value={description}
-                                   onChange={handleDescription}/>
-                            <div className="invalid-feedback">
-                                Job description is required.
-                            </div>
-                        </div>
-                        {showInputError &&
-                            <p className='text-danger'>Please fill in all inputs</p>
-                        }
-                        <div className='d-flex justify-content-evenly'>
-                            {/*<button className='color-theme fw-semibold' onClick={handleCancel}>Cancel</button>*/}
-                            <button type='submit' className='w-auto text-white border-0 bg-theme px-8 py-2 fw-semibold rounded-0'
-                                    onClick={handleSubmit}>Submit
-                            </button>
-                        </div>
+            <form className="needs-validation mt-2" onSubmit={handleSubmit}>
+                <div className="row g-3 d-flex justify-content-center">
+                    <div className="col-sm-6 col-lg-5">
+                        <label htmlFor="company" className="form-label fw-semibold">Company</label>
+                        <input type="text" name='company' value={state.company} onChange={handleChange}
+                               className="form-control rounded-0"
+                               placeholder="Enter company name..."/>
                     </div>
-                </form>
+
+                    <div className="col-sm-6 col-lg-5">
+                        <label className="form-label fw-semibold">Job title</label>
+                        <input type="text" name='title' className="form-control rounded-0" id="jobTitle"
+                               placeholder="Enter job title..." value={state.title} onChange={handleChange}
+                               required/>
+                    </div>
+                    <div className="col-sm-6 col-lg-5">
+                        <label htmlFor="label" className="form-label fw-semibold">Label</label>
+                        <input className="form-control rounded-0 " id="exampleFormControlTextarea1"
+                               name='label'
+                               placeholder="Enter job label: e.g.(Engineering Jobs)..." value={state.label}
+                               onChange={handleChange}/>
+                    </div>
+
+                    <div className="col-sm-6 col-lg-5">
+                        <label htmlFor="location" className="form-label fw-semibold">Location</label>
+                        <input className="form-control rounded-0 " id="exampleFormControlTextarea1"
+                               name='location'
+                               placeholder="Enter job location..." value={state.location}
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="col-sm-6 col-lg-10">
+                        <label htmlFor="description" className="form-label fw-semibold">Description</label>
+                        <textarea className="form-control rounded-0" id="exampleFormControlTextarea1"
+                                  name='description'
+                                  placeholder="Enter job description..." value={state.description}
+                                  onChange={handleChange}
+                                  rows="4"/>
+                    </div>
+                    {showInputError &&
+                    <p className='text-danger text-center'>Please fill in all inputs</p>}
+                    <div className='d-flex justify-content-evenly'>
+                        <button type='submit' className='w-auto text-white bg-theme px-5 border-0 py-2 fw-semibold rounded-0'
+                                onClick={handleSubmit}>Submit
+                        </button>
+                    </div>
+                </div>
+            </form>
             }
         </>
     )
